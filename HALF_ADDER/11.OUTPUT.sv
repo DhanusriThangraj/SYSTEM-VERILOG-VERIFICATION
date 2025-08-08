e4e662e4552cf0e52f27e288c2d9533492dd9ab9
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-08 22:51:37 +0530

    Create 11.OUTPUT.sv

---
+++ HALF_ADDER/11.OUTPUT.sv
@@ -0,0 +1,46 @@
+GENERATOR
+Time=0 a=0 b=1 sum=0 carry=0
+DRIVER
+Time=1 a=0 b=1 sum=0 carry=0
+MONITOR
+Time=2 a=0 b=1 sum=1 carry=0
+SCOREBOARD
+Time=2 a=0 b=1 sum=1 carry=0
+PASS---------------------------PASS
+GENERATOR
+Time=3 a=1 b=1 sum=0 carry=0
+DRIVER
+Time=4 a=1 b=1 sum=0 carry=0
+MONITOR
+Time=5 a=1 b=1 sum=0 carry=1
+SCOREBOARD
+Time=5 a=1 b=1 sum=0 carry=1
+PASS---------------------------PASS
+GENERATOR
+Time=6 a=0 b=1 sum=0 carry=0
+DRIVER
+Time=7 a=0 b=1 sum=0 carry=0
+MONITOR
+Time=8 a=0 b=1 sum=1 carry=0
+SCOREBOARD
+Time=8 a=0 b=1 sum=1 carry=0
+PASS---------------------------PASS
+GENERATOR
+Time=9 a=0 b=1 sum=0 carry=0
+DRIVER
+Time=10 a=0 b=1 sum=0 carry=0
+MONITOR
+Time=11 a=0 b=1 sum=1 carry=0
+SCOREBOARD
+Time=11 a=0 b=1 sum=1 carry=0
+PASS---------------------------PASS
+GENERATOR
+Time=12 a=1 b=1 sum=0 carry=0
+DRIVER
+Time=13 a=1 b=1 sum=0 carry=0
+MONITOR
+Time=14 a=1 b=1 sum=0 carry=1
+SCOREBOARD
+Time=14 a=1 b=1 sum=0 carry=1
+PASS---------------------------PASS
+$finish at simulation time                   15

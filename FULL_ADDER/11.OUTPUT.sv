a42d8be49f942c4e369515c95acb5e249046351c
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-08 22:46:19 +0530

    Create 11.OUTPUT.sv

---
+++ FULL_ADDER/11.OUTPUT.sv
@@ -0,0 +1,23 @@
+GENERATOR
+ Time=0 a=0 b=1 c=1 sum=0 carry=0
+DRIVER
+ Time=1 a=0 b=1 c=1 sum=0 carry=0
+MONITOR
+ Time=2 a=0 b=1 c=1 sum=0 carry=1
+SCOREBOARD
+ Time=2 a=0 b=1 c=1 sum=0 carry=1
+===================PASS======================
+SCOREBOARD
+ Time=2 a=0 b=1 c=1 sum=0 carry=1
+GENERATOR
+ Time=3 a=1 b=1 c=1 sum=0 carry=0
+DRIVER
+ Time=4 a=1 b=1 c=1 sum=0 carry=0
+MONITOR
+ Time=5 a=1 b=1 c=1 sum=1 carry=1
+SCOREBOARD
+ Time=5 a=1 b=1 c=1 sum=1 carry=1
+===================PASS======================
+SCOREBOARD
+ Time=5 a=1 b=1 c=1 sum=1 carry=1
+$finish at simulation time     

0b5ce005b73014a0fd912d9a708a02c6ee6eece7
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-11 00:55:47 +0530

    Create 08.ENVIRONMENT.sv

---
+++ D_FLIPFLOP/08.ENVIRONMENT.sv
@@ -0,0 +1,45 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+
+class environment;
+  generator gen;
+  driver driv;
+  monitor moni;
+  scoreboard score;
+  transaction trans;
+  
+  mailbox gen_to_driv;
+  mailbox moni_to_score;
+  
+  virtual inter interf;
+  event dhanusri;
+  
+  function new (virtual inter interf);
+    this.interf=interf;
+   
+    gen_to_driv=new();
+    moni_to_score=new();
+    
+    gen=new(gen_to_driv);
+    driv=new(interf,gen_to_driv);
+    moni=new(interf,moni_to_score);
+    score=new(moni_to_score);
+    
+    gen.dhanu=this.dhanusri;
+    score.sri=this.dhanusri;
+    
+   endfunction
+  
+  task test();
+    fork
+      gen.run();
+      driv.run();
+      moni.run();
+      score.run();
+    join
+  endtask
+  
+endclass

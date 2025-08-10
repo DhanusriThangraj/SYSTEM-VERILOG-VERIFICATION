7692f93af1429ec09107ae676675227ed65bef90
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-08-11 00:54:40 +0530

    Create 06.MONITOR.sv

---
+++ D_FLIPFLOP/06.MONITOR.sv
@@ -0,0 +1,29 @@
+class monitor;
+  transaction trans;
+  mailbox moni_to_score;
+  virtual inter interf;
+  
+  function new (virtual inter interf,mailbox moni_to_score);
+    this.interf =interf;
+    this.moni_to_score=moni_to_score;   
+  endfunction
+  
+  task run();
+    trans=new();
+    repeat(50) begin
+      
+      @(posedge interf.clk);
+//       #1;
+      //trans.clk=interf.clk;
+      trans.reset=interf.reset;
+      trans.d=interf.d;
+      trans.q=interf.q;
+     
+      moni_to_score.put(trans);
+      trans.display("MONITOR");
+      
+    end
+  endtask
+  
+endclass
+  

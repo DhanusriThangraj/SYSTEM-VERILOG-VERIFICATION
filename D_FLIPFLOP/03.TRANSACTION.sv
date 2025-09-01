88ab5aade530860cd0e1032743c6fe412326c36e
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-09-01 23:17:55 +0530

    Update 03.TRANSACTION.sv

--- D_FLIPFLOP/03.TRANSACTION.sv
+++ D_FLIPFLOP/03.TRANSACTION.sv
@@ -3,7 +3,7 @@ class transaction;
   bit reset;
   rand bit d;
   bit q;
-  
+
   function void display(string flipflop);
     $display("%s",flipflop);
     $display("Time=%0t reset=%b d=%b q=%b",$time,reset,d,q);

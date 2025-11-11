94a6c5bac333b2f577d14377a43957ef1ecf6da2
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-11-11 18:23:41 +0530

    Create 12.OUTPUT.sv

---
+++ FIFO/12.OUTPUT.sv
@@ -0,0 +1,74 @@
+xcelium> source /xcelium23.09/tools/xcelium/files/xmsimrc
+xcelium> run
+GENERATOR
+clk=0	reset=0	write_en=0	read_en=0	data_in=2	data_out=0	full=0	empty=0
+DRIVER
+clk=0	reset=0	write_en=0	read_en=0	data_in=2	data_out=0	full=0	empty=0
+MONITOR
+clk=1	reset=1	write_en=0	read_en=0	data_in=2	data_out=0	full=0	empty=1
+SCOREBOARD
+clk=1	reset=1	write_en=0	read_en=0	data_in=2	data_out=0	full=0	empty=1
+----------------------FIFO PASSED----------------------
+GENERATOR
+clk=0	reset=0	write_en=0	read_en=0	data_in=8	data_out=0	full=0	empty=0
+DRIVER
+clk=0	reset=0	write_en=0	read_en=0	data_in=8	data_out=0	full=0	empty=0
+MONITOR
+clk=1	reset=0	write_en=1	read_en=0	data_in=8	data_out=0	full=0	empty=0
+SCOREBOARD
+clk=1	reset=0	write_en=1	read_en=0	data_in=8	data_out=0	full=0	empty=0
+----------------------FIFO PASSED----------------------
+GENERATOR
+clk=0	reset=0	write_en=0	read_en=0	data_in=a	data_out=0	full=0	empty=0
+DRIVER
+clk=0	reset=0	write_en=0	read_en=0	data_in=a	data_out=0	full=0	empty=0
+MONITOR
+clk=1	reset=0	write_en=1	read_en=0	data_in=a	data_out=0	full=0	empty=0
+SCOREBOARD
+clk=1	reset=0	write_en=1	read_en=0	data_in=a	data_out=0	full=0	empty=0
+----------------------FIFO PASSED----------------------
+GENERATOR
+clk=0	reset=0	write_en=0	read_en=0	data_in=7	data_out=0	full=0	empty=0
+DRIVER
+clk=0	reset=0	write_en=0	read_en=0	data_in=7	data_out=0	full=0	empty=0
+MONITOR
+clk=1	reset=0	write_en=1	read_en=0	data_in=7	data_out=0	full=0	empty=0
+SCOREBOARD
+clk=1	reset=0	write_en=1	read_en=0	data_in=7	data_out=0	full=0	empty=0
+----------------------FIFO PASSED----------------------
+GENERATOR
+clk=0	reset=0	write_en=0	read_en=0	data_in=5	data_out=0	full=0	empty=0
+DRIVER
+clk=0	reset=0	write_en=0	read_en=0	data_in=5	data_out=0	full=0	empty=0
+MONITOR
+clk=1	reset=0	write_en=1	read_en=0	data_in=5	data_out=0	full=0	empty=0
+SCOREBOARD
+clk=1	reset=0	write_en=1	read_en=0	data_in=5	data_out=0	full=0	empty=0
+----------------------FIFO PASSED----------------------
+GENERATOR
+clk=0	reset=0	write_en=0	read_en=0	data_in=4	data_out=0	full=0	empty=0
+DRIVER
+clk=0	reset=0	write_en=0	read_en=0	data_in=4	data_out=0	full=0	empty=0
+MONITOR
+clk=1	reset=0	write_en=1	read_en=0	data_in=4	data_out=0	full=0	empty=0
+SCOREBOARD
+clk=1	reset=0	write_en=1	read_en=0	data_in=4	data_out=0	full=0	empty=0
+----------------------FIFO PASSED----------------------
+GENERATOR
+clk=0	reset=0	write_en=0	read_en=0	data_in=9	data_out=0	full=0	empty=0
+DRIVER
+clk=0	reset=0	write_en=0	read_en=0	data_in=9	data_out=0	full=0	empty=0
+MONITOR
+clk=1	reset=0	write_en=1	read_en=0	data_in=9	data_out=0	full=0	empty=0
+SCOREBOARD
+clk=1	reset=0	write_en=1	read_en=0	data_in=9	data_out=0	full=0	empty=0
+----------------------FIFO PASSED----------------------
+GENERATOR
+clk=0	reset=0	write_en=0	read_en=0	data_in=3	data_out=0	full=0	empty=0
+DRIVER
+clk=0	reset=0	write_en=0	read_en=0	data_in=3	data_out=0	full=0	empty=0
+MONITOR
+clk=1	reset=0	write_en=1	read_en=0	data_in=3	data_out=0	full=0	empty=0
+SCOREBOARD
+clk=1	reset=0	write_en=1	read_en=0	data_in=3	data_out=0	full=0	empty=0
+----------------------FIFO PASSED----------------------
